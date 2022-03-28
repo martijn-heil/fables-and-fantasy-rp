96c2224c82f4a5e06e5cebbfbf8ed7c4408f934f
Martijn Heil <m.heil375@gmail.com>
2022-03-29 01:53:16 +0200

    add some sample code

--- HorseLimits/src/main/kotlin/com/fablesfantasyrp/plugin/horselimits/FablesHorseLimits.kt
+++ HorseLimits/src/main/kotlin/com/fablesfantasyrp/plugin/horselimits/FablesHorseLimits.kt
@@ -1,9 +1,14 @@
 package com.fablesfantasyrp.plugin.horselimits
 
+import org.bukkit.entity.Horse
 import org.bukkit.plugin.java.JavaPlugin
 
 class FablesHorseLimits : JavaPlugin() {
+	val MAX_JUMP_STRENGTH = 0.6
+
 	override fun onEnable() {
 		logger.info("horse limits enabled boi!")
+		val horses = server.worlds.map { it.entities.filterIsInstance<Horse>() }.flatten()
+		horses.forEach { if(it.jumpStrength > MAX_JUMP_STRENGTH) it.jumpStrength = MAX_JUMP_STRENGTH }
 	}
 }
