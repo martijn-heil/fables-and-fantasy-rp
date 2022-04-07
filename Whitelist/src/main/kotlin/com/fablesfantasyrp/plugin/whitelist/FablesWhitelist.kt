4932ce65be271150e99e70e550fcb381cbd26db0
Martijn Heil <m.heil375@gmail.com>
2022-04-07 17:51:19 +0200

    Add blank Whitelist system

---
+++ Whitelist/src/main/kotlin/com/fablesfantasyrp/plugin/whitelist/FablesWhitelist.kt
@@ -0,0 +1,18 @@
+package com.fablesfantasyrp.plugin.whitelist
+
+import org.bukkit.ChatColor.*
+import org.bukkit.plugin.java.JavaPlugin
+
+internal val SYSPREFIX = "${DARK_RED}${BOLD}[${RED}${BOLD}WHITELIST${DARK_RED}${BOLD}]${GRAY}"
+
+class FablesWhitelist : JavaPlugin() {
+
+	override fun onEnable() {
+		instance = this
+
+	}
+
+	companion object {
+		lateinit var instance: FablesWhitelist
+	}
+}
