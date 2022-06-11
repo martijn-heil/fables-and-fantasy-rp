07dd815b947ae52f83371cfae4d7aa1ee6ad78d2
Martijn Heil <m.heil375@gmail.com>
2022-06-11 20:32:07 +0200

    characters: fix permission

--- Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/Permission.kt
+++ Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/Permission.kt
@@ -6,7 +6,7 @@ object Permission {
 	object Command {
 		private const val prefix = Permission.prefix + ".command"
 		const val Updatestats = "${prefix}.updatestats"
-		const val Cardother = "${prefix}.othercard"
+		const val Cardother = "${prefix}.cardother"
 		const val Listcharacters = "${prefix}.listchars"
 	}
 }
