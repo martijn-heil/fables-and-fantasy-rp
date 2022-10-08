a02991ddc67681dcbe2caedf7199f1d3ee1496d5
Martijn Heil <m.heil375@gmail.com>
2022-10-08 15:06:18 +0200

    denizeninterop: add denizenParseTag

--- DenizenInterop/src/main/kotlin/com/fablesfantasyrp/plugin/denizeninterop/utils.kt
+++ DenizenInterop/src/main/kotlin/com/fablesfantasyrp/plugin/denizeninterop/utils.kt
@@ -1,10 +1,14 @@
 package com.fablesfantasyrp.plugin.denizeninterop
 
+import com.denizenscript.denizen.Denizen
 import com.denizenscript.denizencore.objects.ObjectTag
 import com.denizenscript.denizencore.objects.core.MapTag
 import com.denizenscript.denizencore.scripts.ScriptBuilder
 import com.denizenscript.denizencore.scripts.ScriptEntry
+import com.denizenscript.denizencore.scripts.ScriptRegistry
+import com.denizenscript.denizencore.scripts.containers.core.ProcedureScriptContainer
 import com.denizenscript.denizencore.scripts.queues.core.InstantQueue
+import com.denizenscript.denizencore.tags.TagManager
 import com.denizenscript.denizencore.utilities.text.StringHolder
 
 fun denizenRun(task: String, definitions: Map<String, ObjectTag>) {
@@ -26,4 +30,14 @@ fun ex(entry: String) {
 	val queue = InstantQueue("EXFUNCTION")
 	queue.addEntries(scriptEntries)
 	queue.start()
+}
+
+fun denizenParseTag(tag: String, definitions: Map<String, ObjectTag>): ObjectTag {
+	// This is ugly, but I can't figure out how to construct a TagContext
+	val locationGetKey = ScriptRegistry.getScriptContainerAs("location_get_key", ProcedureScriptContainer::class.java)
+	val context = Denizen.instance.coreImplementation.getTagContext(locationGetKey).clone()
+
+	definitions.forEach { context.definitionProvider.addDefinition(it.key, it.value) }
+	val procTag = TagManager.parseTextToTag(tag, context)
+	return procTag.parse(context)
 }
