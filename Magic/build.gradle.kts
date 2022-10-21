02de6f1ffcbf9173718dff42a6f912335be6f721
Martijn Heil <m.heil375@gmail.com>
2022-10-21 12:18:13 +0200

    fix build

--- Magic/build.gradle.kts
+++ Magic/build.gradle.kts
@@ -6,5 +6,5 @@ dependencies {
     // internal module dependencies
     implementation(project(":Utils"))
     implementation(project(":Characters"))
-    implementation("com.gitlab.martijn-heil:NinCommands:-SNAPSHOT")
+    implementation("com.gitlab.martijn-heil:NinCommands:1.0-SNAPSHOT")
 }
