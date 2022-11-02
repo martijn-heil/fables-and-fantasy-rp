37f3d2b5994de4e72e4516f0298d6ffabe35b873
Martijn Heil <m.heil375@gmail.com>
2022-11-03 00:08:32 +0100

    magic: start spell defense implementation

--- Magic/build.gradle.kts
+++ Magic/build.gradle.kts
@@ -15,5 +15,6 @@ dependencies {
     implementation(project(":Chat"))
     implementation(project(":Text"))
     implementation(project(":Gui"))
+    implementation(project(":Targeting"))
     implementation("com.gitlab.martijn-heil:NinCommands:1.0-SNAPSHOT")
 }
