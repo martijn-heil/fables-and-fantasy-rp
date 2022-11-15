9a3b0cc5b35e049fcd850b8eda1928fa2e871664
Martijn Heil <m.heil375@gmail.com>
2022-11-15 22:01:40 +0100

    magic: fix own ability

--- Magic/src/main/kotlin/com/fablesfantasyrp/plugin/magic/command/provider/OwnAbility.kt
+++ Magic/src/main/kotlin/com/fablesfantasyrp/plugin/magic/command/provider/OwnAbility.kt
@@ -1,8 +1,5 @@
 package com.fablesfantasyrp.plugin.magic.command.provider
 
-import com.sk89q.intake.parametric.annotation.Classifier
-
 // A spell available to the sender of the command
-@Classifier
 @Retention
 annotation class OwnAbility
