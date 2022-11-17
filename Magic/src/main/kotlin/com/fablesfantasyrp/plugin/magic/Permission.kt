518db99f5f677c33f9951569f01e1a9994c3003e
Martijn Heil <m.heil375@gmail.com>
2022-11-17 21:00:38 +0100

    magic: fix ability permissions

--- Magic/src/main/kotlin/com/fablesfantasyrp/plugin/magic/Permission.kt
+++ Magic/src/main/kotlin/com/fablesfantasyrp/plugin/magic/Permission.kt
@@ -5,20 +5,20 @@ internal object Permission {
 
 	object Command {
 		private const val prefix = Permission.prefix + ".command"
-		const val Castspell = "${prefix}.castspell"
-		const val Opentear = "${prefix}.opentear"
-		const val Closetear = "${prefix}.closetear"
-		const val Spellbook = "${prefix}.spellbook"
-		const val Resetspellbook = "${prefix}.resetspellbook"
-		const val Setmagicpath = "${prefix}.setmagictype"
-		const val Setmagiclevel = "${prefix}.setmagiclevel"
-		const val Tears = "${prefix}.tears"
+		const val Castspell = "$prefix.castspell"
+		const val Opentear = "$prefix.opentear"
+		const val Closetear = "$prefix.closetear"
+		const val Spellbook = "$prefix.spellbook"
+		const val Resetspellbook = "$prefix.resetspellbook"
+		const val Setmagicpath = "$prefix.setmagictype"
+		const val Setmagiclevel = "$prefix.setmagiclevel"
+		const val Tears = "$prefix.tears"
 
 		object Ability {
 			private const val prefix = Permission.prefix + ".ability"
-			const val List = "${Command.prefix}.list"
-			const val Activate = "${Command.prefix}.activate"
-			const val Deactiviate = "${Command.prefix}.deactivate"
+			const val List = "$prefix.list"
+			const val Activate = "$prefix.activate"
+			const val Deactiviate = "$prefix.deactivate"
 		}
 	}
 }
