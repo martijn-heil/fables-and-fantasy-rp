7aa2b9170135135c29b581eff64da54554fb0d5a
Martijn Heil <m.heil375@gmail.com>
2022-11-22 00:38:51 +0100

    playerinstance: fire PlayerSwitchPlayerInstanceEvent

--- PlayerInstance/src/main/kotlin/com/fablesfantasyrp/plugin/playerinstance/PlayerInstanceManager.kt
+++ PlayerInstance/src/main/kotlin/com/fablesfantasyrp/plugin/playerinstance/PlayerInstanceManager.kt
@@ -1,12 +1,17 @@
 package com.fablesfantasyrp.plugin.playerinstance
 
 import com.fablesfantasyrp.plugin.playerinstance.data.entity.PlayerInstance
+import org.bukkit.Server
 import org.bukkit.entity.Player
 
-class PlayerInstanceManager {
+class PlayerInstanceManager(private val server: Server) {
 	private val currentInstances = HashMap<Player, PlayerInstance>()
 
 	fun getCurrentForPlayer(player: Player) = currentInstances[player]
-	fun setCurrentForPlayer(player: Player, playerInstance: PlayerInstance) { currentInstances[player] = playerInstance }
+	fun setCurrentForPlayer(player: Player, playerInstance: PlayerInstance) {
+		val event = PlayerSwitchPlayerInstanceEvent(player, getCurrentForPlayer(player), playerInstance)
+		server.pluginManager.callEvent(event)
+		currentInstances[player] = playerInstance
+	}
 	fun stopTracking(player: Player) { currentInstances.remove(player) }
 }
