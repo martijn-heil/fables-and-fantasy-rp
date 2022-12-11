fb737c31c41ceed42761b21c8688b3ed384a19e0
Martijn Heil <m.heil375@gmail.com>
2022-12-11 19:20:55 +0100

    characters: remove old updatestats command

--- Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/command/Commands.kt
+++ Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/command/Commands.kt
@@ -33,35 +33,6 @@ import java.time.Duration
 import java.time.Instant
 
 class Commands(private val plugin: SuspendingJavaPlugin) {
-	@Command(aliases = ["updatestats"], desc = "Update a player's character stats")
-	@Require(Permission.Command.Updatestats)
-	fun updatestats(@Sender sender: Player, target: CharacterData) {
-		val minimums = target.race.boosters + CharacterStats(2U, 2U, 2U, 2U)
-		var initialSliderValues = target.stats - minimums
-		if (initialSliderValues.strength > 8U ||
-				initialSliderValues.defense > 8U ||
-				initialSliderValues.agility > 8U ||
-				initialSliderValues.intelligence > 8u) {
-			sender.sendMessage("$SYSPREFIX Detected that you will be editing legacy player stats, starting with a clean slate.")
-			initialSliderValues = CharacterStats(0U, 0U, 0U, 0U)
-		}
-
-		val gui = CharacterStatsGui(FablesCharacters.instance, minimums, "#${target.id} ${target.name}'s stats",
-				initialSliderValues)
-
-		plugin.launch {
-			val result = gui.execute(sender)
-			denizenRun("characters_set_new_stats", mapOf(
-					Pair("player", PlayerTag(target.player)),
-					Pair("id", ElementTag(target.id.toLong())),
-					Pair("strength", ElementTag(result.strength.toInt())),
-					Pair("defense", ElementTag(result.defense.toInt())),
-					Pair("agility", ElementTag(result.agility.toInt())),
-					Pair("intelligence", ElementTag(result.intelligence.toInt()))
-			))
-		}
-	}
-
 	class Characters(private val plugin: SuspendingJavaPlugin,
 					 private val playerInstanceRepository: PlayerInstanceRepository) {
 		@Command(aliases = ["new"], desc = "Create a new character!")
