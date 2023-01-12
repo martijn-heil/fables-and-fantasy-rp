a50dfe3bd29675af6c550b253c2db6b0f81ae263
Martijn Heil <m.heil375@gmail.com>
2023-01-12 02:09:18 +0100

    player instance: add missing sync

--- PlayerInstance/src/main/kotlin/com/fablesfantasyrp/plugin/playerinstance/data/entity/EntityPlayerInstanceRepositoryImpl.kt
+++ PlayerInstance/src/main/kotlin/com/fablesfantasyrp/plugin/playerinstance/data/entity/EntityPlayerInstanceRepositoryImpl.kt
@@ -12,5 +12,8 @@ class EntityPlayerInstanceRepositoryImpl<C>(child: C) :
 			  C: MutableRepository<PlayerInstance>,
 			  C: HasDirtyMarker<PlayerInstance>,
 			  C: PlayerInstanceRepository {
-	override fun forOwner(offlinePlayer: OfflinePlayer): Collection<PlayerInstance> = child.forOwner(offlinePlayer).mapNotNull { this.forId(it.id) }
+	override fun forOwner(offlinePlayer: OfflinePlayer): Collection<PlayerInstance> {
+		this.saveAllDirty()
+		return child.forOwner(offlinePlayer).mapNotNull { this.forId(it.id) }
+	}
 }
