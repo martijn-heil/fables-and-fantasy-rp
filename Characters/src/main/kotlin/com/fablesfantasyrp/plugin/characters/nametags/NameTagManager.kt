2bb9bb5495885c1008ca6806bbe00831fa64b448
Martijn Heil <m.heil375@gmail.com>
2023-02-20 00:10:11 +0100

    fix nametags

--- Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/nametags/NameTagManager.kt
+++ Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/nametags/NameTagManager.kt
@@ -3,12 +3,16 @@ package com.fablesfantasyrp.plugin.characters.nametags
 import com.fablesfantasyrp.plugin.characters.data.entity.EntityCharacterRepository
 import com.fablesfantasyrp.plugin.denizeninterop.dFlags
 import com.fablesfantasyrp.plugin.profile.ProfileManager
+import com.fablesfantasyrp.plugin.utils.EDEN
 import me.neznamy.tab.api.TabAPI
+import org.bukkit.GameMode
 import org.bukkit.entity.Player
+import java.util.*
 
 class NameTagManager(private val profileManager: ProfileManager,
 					 private val characters: EntityCharacterRepository) {
 	private lateinit var tapi: TabAPI
+	private val ROBIN = UUID.fromString("6d147f5e-2509-4f92-82c6-86b57058e916")
 
 	fun start() {
 		tapi = TabAPI.getInstance()
@@ -21,8 +25,10 @@ class NameTagManager(private val profileManager: ProfileManager,
 				val targetCharacter = targetProfile?.let { characters.forProfile(it) }
 				val showCharacterNames = observer.dFlags.getFlagValue("characters_togglenames")
 				  ?.let { it.asElement().asBoolean() } ?: true
-
-				if (showCharacterNames && targetCharacter != null) {
+				if (target.uniqueId == ROBIN && target.gameMode == GameMode.CREATIVE &&
+						target.isFlying && target.flySpeed >= 0.3 && target.location.world == EDEN) {
+					"'Fighter Jet' Robin"
+				} else if (showCharacterNames && targetCharacter != null) {
 					targetCharacter.name
 				} else {
 					target.name
