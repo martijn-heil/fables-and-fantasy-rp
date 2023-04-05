f5f6e9ef600ffe40ee37e68c1c2b77a247dc1eab
Martijn Heil <m.heil375@gmail.com>
2023-04-05 14:53:25 +0200

    party: exclude a few party colors

--- Party/src/main/kotlin/com/fablesfantasyrp/plugin/party/PartyColor.kt
+++ Party/src/main/kotlin/com/fablesfantasyrp/plugin/party/PartyColor.kt
@@ -9,13 +9,10 @@ enum class PartyColor(val chatColor: ChatColor) {
 	 DARK_RED(ChatColor.DARK_RED),
 	 DARK_PURPLE(ChatColor.DARK_PURPLE),
 	 GOLD(ChatColor.GOLD),
-	 GRAY(ChatColor.GRAY),
-	 DARK_GRAY(ChatColor.DARK_GRAY),
 	 BLUE(ChatColor.BLUE),
 	 GREEN(ChatColor.GREEN),
 	 AQUA(ChatColor.AQUA),
 	 RED(ChatColor.RED),
 	 LIGHT_PURPLE(ChatColor.LIGHT_PURPLE),
 	 YELLOW(ChatColor.YELLOW),
-	 WHITE(ChatColor.WHITE)
 }
