1ea0e0409bbd4d3776de1e5ee01409805ff35c8c
Martijn Heil <m.heil375@gmail.com>
2023-04-05 20:51:20 +0200

    attempt to optimize horselimits

--- HorseLimits/src/main/kotlin/com/fablesfantasyrp/plugin/horselimits/FablesHorseLimits.kt
+++ HorseLimits/src/main/kotlin/com/fablesfantasyrp/plugin/horselimits/FablesHorseLimits.kt
@@ -2,16 +2,18 @@ package com.fablesfantasyrp.plugin.horselimits
 
 import org.bukkit.entity.Horse
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.stream.Collectors
 
 class FablesHorseLimits : JavaPlugin() {
 	val MAX_JUMP_STRENGTH = 0.6
 
 	override fun onEnable() {
 		server.scheduler.scheduleSyncRepeatingTask(this, {
-			server.worlds.asSequence()
-					.map { it.entities.asSequence().filterIsInstance<Horse>() }
-					.flatten()
-					.forEach { if(it.jumpStrength > MAX_JUMP_STRENGTH) it.jumpStrength = MAX_JUMP_STRENGTH }
+			server.worlds.parallelStream()
+				.flatMap { it.entities.parallelStream() }
+				.filter { it is Horse && it.jumpStrength > MAX_JUMP_STRENGTH }
+				.collect(Collectors.toList())
+				.forEach { (it as Horse).jumpStrength = MAX_JUMP_STRENGTH }
 		}, 1, 1)
 	}
 }
