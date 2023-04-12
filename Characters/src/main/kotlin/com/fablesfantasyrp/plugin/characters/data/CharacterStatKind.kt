c6160bd1bd2260b5f09c53434c5e8f36b5ab250a
Martijn Heil <m.heil375@gmail.com>
2023-04-12 13:43:41 +0200

    rolls: remove roll modifier cap

--- Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/data/CharacterStatKind.kt
+++ Characters/src/main/kotlin/com/fablesfantasyrp/plugin/characters/data/CharacterStatKind.kt
@@ -1,13 +1,11 @@
 package com.fablesfantasyrp.plugin.characters.data
 
-import kotlin.math.min
-
 enum class CharacterStatKind {
 	STRENGTH,
 	DEFENSE,
 	AGILITY,
 	INTELLIGENCE;
 
-	fun getRollModifierFor(statValue: UInt): Int = (min(statValue, 12U).toInt() / 2 * 2 - 6) / 2
+	fun getRollModifierFor(statValue: UInt): Int = (statValue.toInt() / 2 * 2 - 6) / 2
 	override fun toString() = name.lowercase()
 }
