725f48826513cb17183fe314a360b61db7200204
Martijn Heil <m.heil375@gmail.com>
2023-04-16 00:04:36 +0200

    whitelist: 2% performance optimization

--- Whitelist/src/main/kotlin/com/fablesfantasyrp/plugin/whitelist/WhitelistMonitor.kt
+++ Whitelist/src/main/kotlin/com/fablesfantasyrp/plugin/whitelist/WhitelistMonitor.kt
@@ -3,33 +3,53 @@ package com.fablesfantasyrp.plugin.whitelist
 import com.fablesfantasyrp.plugin.utils.every
 import com.fablesfantasyrp.plugin.whitelist.event.WhitelistAddedPlayerEvent
 import com.fablesfantasyrp.plugin.whitelist.event.WhitelistRemovedPlayerEvent
+import com.github.shynixn.mccoroutine.bukkit.launch
+import com.github.shynixn.mccoroutine.bukkit.scope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
+import org.bukkit.craftbukkit.v1_19_R1.CraftServer
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 import kotlin.time.Duration.Companion.milliseconds
 
 class WhitelistMonitor(private val plugin: JavaPlugin) {
 	private val server = plugin.server
 	private var isStopped = false
-	private var lastWhitelist = server.whitelistedPlayers.toHashSet()
+	private lateinit var lastWhitelist: Set<UUID>
 
 	fun start() {
-		every(plugin, 50.milliseconds) {
-			if (isStopped) { this.cancel(); return@every }
-			val whitelist = server.whitelistedPlayers.toHashSet()
+		plugin.launch {
+			lastWhitelist = getWhitelist().await()
 
-			// These .minus() operations take about 0.02 ms or less for each to run on our production server hardware
-			// And considering that they're executed off the main thread, we really shouldn't worry about it.
-			val removedPlayers 	= async { lastWhitelist.minus(whitelist) }
-			val addedPlayers 	= async { whitelist.minus(lastWhitelist) }
+			every(plugin, 50.milliseconds) {
+				if (isStopped) { this.cancel(); return@every }
+				val whitelist = getWhitelist().await()
 
-			removedPlayers.await().forEach { server.pluginManager.callEvent(WhitelistRemovedPlayerEvent(it)) }
-			addedPlayers.await().forEach { server.pluginManager.callEvent(WhitelistAddedPlayerEvent(it)) }
-			lastWhitelist = whitelist
+				// These .minus() operations take about 0.02 ms or less for each to run on our production server hardware
+				// And considering that they're executed off the main thread, we really shouldn't worry about it.
+				val removedPlayers 	= async { lastWhitelist.minus(whitelist) }
+				val addedPlayers 	= async { whitelist.minus(lastWhitelist) }
+
+				removedPlayers.await().forEach {
+					server.pluginManager.callEvent(WhitelistRemovedPlayerEvent(server.getOfflinePlayer(it)))
+				}
+
+				addedPlayers.await().forEach {
+					server.pluginManager.callEvent(WhitelistAddedPlayerEvent(server.getOfflinePlayer(it)))
+				}
+
+				lastWhitelist = whitelist
+			}
 		}
 	}
 
 	fun stop() {
 		isStopped = true
+	}
+
+	private fun getWhitelist(): Deferred<Set<UUID>> {
+		val whitelist = (server as CraftServer).handle.whiteList.values
+		return plugin.scope.async { whitelist.asSequence().mapNotNull { it.user?.id }.toHashSet() }
 	}
 }
