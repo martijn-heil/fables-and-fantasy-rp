7d73816e4c4126d6cd6533ef7dddb06d32352fcb
Martijn Heil <m.heil375@gmail.com>
2023-10-22 13:20:29 +0200

    web: remove demo endpoint

--- Web/src/main/kotlin/com/fablesfantasyrp/plugin/web/plugins/Routing.kt
+++ Web/src/main/kotlin/com/fablesfantasyrp/plugin/web/plugins/Routing.kt
@@ -1,25 +1,14 @@
 package com.fablesfantasyrp.plugin.web.plugins
 
-import com.fablesfantasyrp.plugin.web.PLUGIN
 import com.fablesfantasyrp.plugin.web.loaders.WebRoutingLoader
-import com.github.shynixn.mccoroutine.bukkit.minecraftDispatcher
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.withContext
 import org.koin.core.context.GlobalContext
 
 fun Application.configureRouting() {
 	routing {
 		authenticate("auth-bearer") {
-			get("/time") {
-				val time = withContext(PLUGIN.minecraftDispatcher) {
-					PLUGIN.server.worlds.find { it.name == "world" }!!.time
-				}
-				call.respondText("Current time in world: $time")
-			}
-
 			GlobalContext.get().getAll<WebRoutingLoader>().forEach { it.load()(this) }
 		}
 	}
