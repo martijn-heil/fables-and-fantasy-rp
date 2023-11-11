138151e1c459b5e9c88558c7a5a76ed81432a7c9
Martijn Heil <m.heil375@gmail.com>
2023-11-11 12:09:17 +0100

    fix build

--- Bell/src/main/kotlin/com/fablesfantasyrp/plugin/bell/command/provider/BellProvider.kt
+++ Bell/src/main/kotlin/com/fablesfantasyrp/plugin/bell/command/provider/BellProvider.kt
@@ -1,9 +1,9 @@
 package com.fablesfantasyrp.plugin.bell.command.provider
 
 import com.fablesfantasyrp.plugin.bell.domain.entity.Bell
-import com.fablesfantasyrp.plugin.bell.data.entity.EntityBellRepository
+import com.fablesfantasyrp.plugin.bell.domain.repository.BellRepository
 import com.fablesfantasyrp.plugin.database.command.SimpleNamedEntityProvider
 
-class BellProvider(bells: EntityBellRepository) : SimpleNamedEntityProvider<Bell, EntityBellRepository>(bells) {
+class BellProvider(bells: BellRepository) : SimpleNamedEntityProvider<Bell, BellRepository>(bells) {
 	override val entityName: String = "Bell"
 }
