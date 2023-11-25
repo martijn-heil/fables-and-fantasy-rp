b107f28d749a5811cf42ce7e19ef915a824c28f8
Martijn Heil <m.heil375@gmail.com>
2023-11-25 11:49:03 +0100

    lodestones: allow staff character access to all lodestones

--- plugin/extra/Lodestones/src/main/kotlin/com/fablesfantasyrp/plugin/lodestones/LodestoneAuthorizerImpl.kt
+++ plugin/extra/Lodestones/src/main/kotlin/com/fablesfantasyrp/plugin/lodestones/LodestoneAuthorizerImpl.kt
@@ -1,6 +1,7 @@
 package com.fablesfantasyrp.plugin.lodestones
 
 import com.fablesfantasyrp.plugin.characters.domain.repository.CharacterRepository
+import com.fablesfantasyrp.plugin.characters.isStaffCharacter
 import com.fablesfantasyrp.plugin.lodestones.domain.entity.Lodestone
 import com.fablesfantasyrp.plugin.lodestones.domain.repository.CharacterLodestoneRepository
 import com.fablesfantasyrp.plugin.profile.data.entity.Profile
@@ -9,6 +10,6 @@ class LodestoneAuthorizerImpl(private val characters: CharacterRepository,
 							  private val characterLodestones: CharacterLodestoneRepository) : LodestoneAuthorizer {
 	override fun mayWarpTo(who: Profile?, lodestone: Lodestone): Boolean {
 		val character = who?.let { characters.forProfile(who) } ?: return true
-		return lodestone.isPublic || characterLodestones.forCharacter(character).contains(lodestone)
+		return lodestone.isPublic || character.isStaffCharacter || characterLodestones.forCharacter(character).contains(lodestone)
 	}
 }
