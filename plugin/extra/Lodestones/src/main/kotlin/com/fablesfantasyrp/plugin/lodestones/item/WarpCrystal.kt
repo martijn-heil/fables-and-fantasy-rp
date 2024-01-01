59c193dd19e31a6b2fd7344313ab7c3c39eb13f1
Martijn Heil <m.heil375@gmail.com>
2024-01-01 20:15:45 +0100

    lodestones: ignore astools nether star tool

--- plugin/extra/Lodestones/src/main/kotlin/com/fablesfantasyrp/plugin/lodestones/item/WarpCrystal.kt
+++ plugin/extra/Lodestones/src/main/kotlin/com/fablesfantasyrp/plugin/lodestones/item/WarpCrystal.kt
@@ -1,14 +1,29 @@
 package com.fablesfantasyrp.plugin.lodestones.item
 
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 object WarpCrystal {
 	fun matches(item: ItemStack): Boolean {
-		return item.type == Material.NETHER_STAR
+		return item.type == Material.NETHER_STAR && !isArmorStandToolsTool(item)
 	}
 
 	fun create(): ItemStack {
 		return ItemStack(Material.NETHER_STAR)
+	}
+
+	/**
+	 * Check whether this nether star is the astools tool
+	 * astools is a plugin that lets you modify armor stands using a nice interface.
+	 * It makes you enter it's astools mode and takes control over your inventory, putting a nether star
+	 * among other things in it to let you control aspects of the subject armorstand.
+	 * We don't want to match the astools nether star when checking whether a nether star is a warp crystal.
+	 */
+	private fun isArmorStandToolsTool(item: ItemStack): Boolean {
+		val serializer = PlainTextComponentSerializer.plainText()
+		val displayName = item.itemMeta.displayName() ?: return false
+		val name = serializer.serialize(displayName).trim()
+		return name == "GUI Multi-Tool"
 	}
 }
