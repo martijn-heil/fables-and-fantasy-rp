5faaa6a50d725f965d0e41f8311f27245547311b
Martijn Heil <m.heil375@gmail.com>
2024-01-07 20:43:28 +0100

    tools: reset pweather & ptime on join

--- plugin/core/Tools/src/main/kotlin/com/fablesfantasyrp/plugin/tools/ToolsListener.kt
+++ plugin/core/Tools/src/main/kotlin/com/fablesfantasyrp/plugin/tools/ToolsListener.kt
@@ -4,11 +4,18 @@ import com.fablesfantasyrp.plugin.utils.SPAWN
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerRespawnEvent
 
 class ToolsListener : Listener {
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
 	fun onPlayerRespawn(e: PlayerRespawnEvent) {
 		e.respawnLocation = SPAWN
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	fun onPlayerJoin(e: PlayerJoinEvent) {
+		e.player.resetPlayerWeather()
+		e.player.resetPlayerTime()
 	}
 }
